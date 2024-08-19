d474755b24f1fc1dc4fcde12c83aa898bb26efc1
aleksa <zabumaphu@gmail.com>
2024-08-19 19:06:42 -0400

    Initial keywords, seperators, and litterals added.

--- unamed-lang/main.c
+++ unamed-lang/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum{
+    SEMI,
+    OPEN_PAREN,
+    CLOSE_PAREN,
+} TokenTypeSeperator;
+
+typedef enum{
+    EXIT,
+} TokenTypeKeyword;
+typedef enum{
+    INT,
+} TokenTypeLiteral;
+
 int main(){
     printf("Hello, world!\n");
     return 0;
