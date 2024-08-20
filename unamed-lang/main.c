98d22ab9aa47760e57d6a783d55a21cf0c83000d
aleksa <zabumaphu@gmail.com>
2024-08-19 22:04:21 -0400

    Int Tokens Finished.

--- unamed-lang/main.c
+++ unamed-lang/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef enum{
     SEMI,
@@ -24,14 +25,52 @@ typedef struct{
 
 typedef struct{
     TypeLiteral type;
-    int value;
+    char *value;
 } TokenLiteral;
 
 
+TokenLiteral *generate_number(char current, FILE *file){
+    TokenLiteral *token = malloc(sizeof(TokenLiteral));
+    token->type = INT;
+    char *value = malloc(sizeof(char) * 8);
+    int value_index = 0;
+    while(isdigit(current) && current != EOF){
+        if(!isdigit(current)){
+            break;
+        }
+        value[value_index] = current;
+        value_index++;
+        current = fgetc(file);
+    }
+    token->value = value;
+    return(token);
+}
+
+void lexer(FILE *file){
+    char current = fgetc(file);
+
+    while(current != EOF){
+        if(current == ';'){
+            printf("FOUND SEMICOLON\n");
+        } else if (current == '('){
+            printf("FOUND OPEN PAREN\n");
+        } else if (current == ')'){
+            printf("FOUND CLOSED PAREN\n");
+        } else if (isdigit(current)){
+            TokenLiteral *test_token = generate_number(current, file);
+            printf("TEST TOKEN VALUE: %s\n", test_token->value);
+        } else if (isalpha(current)){
+            printf("Found CHARACTER %c\n", current);
+        }
+        current = fgetc(file);
+    }
+}
+
+
 int main(){
-    TokenLiteral token;
-    token.type = INT;
-    token.value = 5;
-    printf("%d\n", token.value);
+    FILE *file;
+    file = fopen("test.unn", "r");
+
+    lexer(file);
     return 0;
 }
