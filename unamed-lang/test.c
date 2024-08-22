c26abfa80824deac95dd8bd3faf74adedee0645b
aleksa <zabumaphu@gmail.com>
2024-08-21 22:13:59 -0400

    File reading test program finished.

--- unamed-lang/test.c
+++ unamed-lang/test.c
@@ -8,13 +8,20 @@ int main(){
 
     file = fopen("test.unn", "r");
 
+    if(!file){
+        printf("ERROR: COULD NOT OPEN FILE\n");
+        exit(1);
+    }
+
     fseek(file, 0, SEEK_END);
     length = ftell(file);
     fseek(file, 0 , SEEK_SET);
-    buffer = malloc(length);
+    buffer = malloc(length + 1);
 
     fread(buffer, 1, length, file);
-    printf("%c\n", *buffer);
+    buffer[length] = '\0';
+
+    printf("%s\n", buffer);
     fclose(file);
 
     return 0;
